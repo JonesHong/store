907be1be5f6ae7c8f509ba2a535176130f18de44
Joneshong <latte831104@gmail.com>
2022-09-15 00:26:45 +0800

    🐞 fix(file-system change back to fs):

--- src/only_backend/fs_extandtion.ts
+++ src/only_backend/fs_extandtion.ts
@@ -1,52 +1,57 @@
-// import { existsSync, mkdirSync, WriteFileOptions, writeFileSync } from "file-system";
+// import { existsSync, mkdirSync, WriteFileOptions, writeFileSync } from "fs";
 
-// var fs = require("file-system");
-import { DateTime } from "luxon";
-import { envType } from "../common/env_checker";
-import { Logger } from "../common/logger";
-const service_name: String = "fs_extandtion";
+// var fs = require("fs");
+import { DateTime } from 'luxon';
+import { envType } from '../common/env_checker';
+import { Logger } from '../common/logger';
+const service_name: String = 'fs_extandtion';
 const isFolderPathExist = (folderPath): boolean => {
-    if (envType !== "nodejs") return;
-    var fs = require("file-system");
+  if (envType !== 'nodejs') return;
+  var fs = require('fs');
 
-    let _payload = fs.existsSync(folderPath);
-    let _beforeExec = DateTime.now();
-    if (!_payload) {
-        fs.mkdirSync(folderPath);
-        let _afterExec = DateTime.now();
-        let execTime = _afterExec.diff(_beforeExec, "milliseconds").toMillis();
-        Logger.log(service_name, `Create '${folderPath}'!`, { execTime });
-    } else {
-        let _afterExec = DateTime.now();
-        let execTime = _afterExec.diff(_beforeExec, "milliseconds").toMillis();
-        Logger.warn(service_name, `FolderPath '${folderPath}' is existed!`, { execTime })
-    }
-    return _payload;
+  let _payload = fs.existsSync(folderPath);
+  let _beforeExec = DateTime.now();
+  if (!_payload) {
+    fs.mkdirSync(folderPath);
+    let _afterExec = DateTime.now();
+    let execTime = _afterExec.diff(_beforeExec, 'milliseconds').toMillis();
+    Logger.log(service_name, `Create '${folderPath}'!`, { execTime });
+  } else {
+    let _afterExec = DateTime.now();
+    let execTime = _afterExec.diff(_beforeExec, 'milliseconds').toMillis();
+    Logger.warn(service_name, `FolderPath '${folderPath}' is existed!`, {
+      execTime,
+    });
+  }
+  return _payload;
 };
 
-import { WriteFileOptions } from "node:fs";
+import { WriteFileOptions } from 'fs';
 // if(envType == "nodejs")
-// import fs = require("file-system");
+// import fs = require("fs");
 // type WriteFileOptions = fs.WriteFileOptions
-const isFilePathExist = (filePath, data: any, options?: WriteFileOptions): boolean => {
-    if (envType !== "nodejs") return;
-    var fs = require("file-system");
-    let _payload = fs.existsSync(filePath);
-    let _beforeExec = DateTime.now();
-    if (!_payload) {
-        fs.writeFileSync(filePath, data, options);
-        let _afterExec = DateTime.now();
-        let execTime = _afterExec.diff(_beforeExec, "milliseconds").toMillis();
-        Logger.log(service_name, `Create '${filePath}'!`, { execTime });
-    } else {
-        let _afterExec = DateTime.now();
-        let execTime = _afterExec.diff(_beforeExec, "milliseconds").toMillis();
-        Logger.warn(service_name, `FilePath '${filePath}' is existed!`, { execTime })
-    }
-    return _payload;
+const isFilePathExist = (
+  filePath,
+  data: any,
+  options?: WriteFileOptions
+): boolean => {
+  if (envType !== 'nodejs') return;
+  var fs = require('fs');
+  let _payload = fs.existsSync(filePath);
+  let _beforeExec = DateTime.now();
+  if (!_payload) {
+    fs.writeFileSync(filePath, data, options);
+    let _afterExec = DateTime.now();
+    let execTime = _afterExec.diff(_beforeExec, 'milliseconds').toMillis();
+    Logger.log(service_name, `Create '${filePath}'!`, { execTime });
+  } else {
+    let _afterExec = DateTime.now();
+    let execTime = _afterExec.diff(_beforeExec, 'milliseconds').toMillis();
+    Logger.warn(service_name, `FilePath '${filePath}' is existed!`, {
+      execTime,
+    });
+  }
+  return _payload;
 };
 
-export {
-    isFolderPathExist,
-    isFilePathExist
-}
+export { isFolderPathExist, isFilePathExist };
