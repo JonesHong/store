f5581d6bee94714919c41d3d30976d5640e6b401
Joneshong <latte831104@gmail.com>
2022-09-17 21:51:15 +0800

    🐞 fix(Selector & RelationStore):
    doc

---
+++ doc/OUTLOOK.md
@@ -0,0 +1,5 @@
+# 未來展望
+
+- entity 可以設定暫存數量與時間，以及暫存邏輯。
+- 以 redis 等技術，Micro-service 可以共同 share build 好的 Store
+- 可以設定排程，讓 Action 置後觸發
--- src/lib/@felangel/bloc/src/errors/event-stream-closed.error.ts
+++
@@ -1,5 +0,0 @@
-export class EventStreamClosedError extends Error {
-  constructor() {
-    super('cannot add new events after calling close')
-  }
-}
