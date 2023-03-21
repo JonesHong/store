91c11420857ae51d879b8b420cf0380f15808ef4
JonesHong <latte831104@gmail.com>
2023-03-21 14:03:03 +0800

    🐞 fix(selector):

--- src/common/selector.ts
+++ src/common/selector.ts
@@ -2,6 +2,8 @@ import { Bloc } from '@felangel/bloc';
 import * as _ from 'lodash';
 import { asapScheduler, combineLatest, Observable, Subscription, zip } from 'rxjs';
 import { filter, map, mergeMap, tap } from 'rxjs/operators';
+import { inspect } from 'util';
+import { Entity } from './entity';
 import { envType } from './env_checker';
 import { Settlement } from './interface/store.interface';
 import { Logger } from './logger';
@@ -41,25 +43,9 @@ export function createFeatureSelector<T>(featureName: string): Observable<T> {
   return stream$;
 }
 
-// console.warn("createRelationSelector", "saojasioasj")
 export function createRelationSelector<T>(featureName: string): Observable<T> {
-
-  // console.warn("featureName", featureName)
   let stream$: Observable<T> = new Observable((observer) => {
     let subscription: Subscription = new Subscription();
-    // console.warn("this.Store['withRelation'] ", !!this.Store['withRelation'][featureName])
-    // if (!this.Store['withRelation'][featureName]) {
-
-
-    //   let _logger = Logger.error(
-    //     'createRelationSelector',
-    //     `Can't find the name ${featureName} of state in Store.withRelation.`,
-    //     { isPrint: Main.printMode !== "none" }
-    //   );
-    //   if (envType == 'browser' && _logger['options']['isPrint'])
-    //     console.error(_logger['_str']);
-
-    // }  
     if (!this.Store['_reducers'][featureName]) {
       let _logger = Logger.error(
         'createFeatureSelector',
@@ -68,16 +54,13 @@ export function createRelationSelector<T>(featureName: string): Observable<T> {
       );
       if (envType == 'browser' && _logger['options']['isPrint'])
         console.error(_logger['_str']);
-
     } else {
-      // console.error(2222222)
       // 第一次
-      observer.next(this.Store['withRelation'][featureName])
+      observer.next(this.Store['withRelation'][featureName]);
       let withRelationSub = this.Store['withRelation$']
         .pipe(
         // filter(withRelation => !!withRelation && !!withRelation['_']['settlement'] && withRelation['_']['settlement']['reducerName'] == featureName),
       )
-        // .subscribe(([settlement, withRelation]) => {
         .subscribe(withRelation => {
           observer.next(withRelation[featureName]);
         })
@@ -108,104 +91,81 @@ export const createSelector = (
 };
 
 
-/**
- * 適用於舊版的 relationConfigOptions，
- * 重新架 RelationConfigTable以後這段將被棄用
- * @param state 
- * @param parameter 
- * @returns 
- */
 export const selectRelevanceEntity = (
   state,
   parameter: { key: string; value: any }
 ) => {
-  let entities: [string, {}][] = Object.entries(state['entities']);
+  let entities: Entity[] = Object.values(state['entities']);
   let payload = [];
-  // await Promise.all(
   entities.map((entity) => {
-    let entityId = entity[0], // e.g.: "u-1"
-      entityValue = entity[1]; // e.g.: {id:"u-1", name:"Jones"}
-    if (!!!entityValue[parameter['key']]) {
-      Logger.warn(
-        `selectRelevanceEntity<${entityId}>`,
-        `There is no ${parameter['key']} in this entity.`
+    if (!!!entity[parameter['key']]) {
+
+      let _logger = Logger.warn(
+        `selectRelevanceEntity<${entity['id']}>`,
+        `There is no ${parameter['key']} in this entity(${entity['_name']}).`,
+        { isPrint: Main.printMode !== "none" }
       );
-      // console.warn(
-      //   `[Error/selectRelevanceEntity<${entity[0]}>] There is no ${parameter['key']} in this entity.`
-      // );
-      parameter['key'] = parameter['key'].substring(
-        0,
-        parameter['key'].length - 2
-      );
-      if (!!!entityValue[parameter['key']]) return;
+      if (envType == 'browser' && _logger['options']['isPrint'])
+        console.warn(_logger['_str']);
     }
 
-    let typeofParameterKey = typeof entityValue[parameter['key']];
+    let typeofParameterKey = typeof entity[parameter['key']];
 
     switch (typeofParameterKey) {
       case 'string':
-        if (entityValue[parameter['key']] == parameter['value']) {
-          payload.push(entityValue);
+        if (entity[parameter['key']] == parameter['value']) {
+          payload.push(entity);
         }
-        if (
-          typeof parameter['value'] == 'object' &&
-          entityValue[parameter['key']] == parameter['value']['id']
-        )
-          payload.push(entityValue);
-        // console.log(111,22,entityValue[parameter['key']], parameter['value'], payload)
         break;
       case 'object':
-        if (Array.isArray(entityValue[parameter['key']])) {
-          let parameterIdList;
-          const includesObj = (element) => typeof element === 'object';
+        if (Array.isArray(entity[parameter['key']])) {
+          // let parameterIdList;
+          // const includesObj = (element) => typeof element === 'object';
 
-          entityValue[parameter['key']].some(includesObj)
-            ? (parameterIdList = entityValue[parameter['key']].map(
-              (entity) => entity.id
-            ))
-            : (parameterIdList = entityValue[parameter['key']]);
-          parameterIdList.map((item) => {
-            switch (typeof item) {
-              case 'string':
-                if (item == parameter['value']) {
-                  payload.push(entityValue);
-                }
-                break;
-              default:
-                Logger.error(
-                  'selectRelevanceEntity',
-                  `There doesn't handle type(${typeof item}).\n${entityValue[parameter['key']]
-                  }\n`
-                );
-                // console.error(
-                //   `[Error/selectRelevanceEntity] There doesn't handle type(${typeof item}).\n${entityValue[parameter['key']]
-                //   }\n`
-                // );
-                break;
-            }
-          });
+          // entityValue[parameter['key']].some(includesObj)
+          //   ? (parameterIdList = entityValue[parameter['key']].map(
+          //     (entity) => entity.id
+          //   ))
+          //   : (parameterIdList = entityValue[parameter['key']]);
+          // parameterIdList.map((item) => {
+          //   switch (typeof item) {
+          //     case 'string':
+          //       if (item == parameter['value']) {
+          //         payload.push(entityValue);
+          //       }
+          //       break;
+          //     default:
+          //       Logger.error(
+          //         'selectRelevanceEntity',
+          //         `There doesn't handle type(${typeof item}).\n${entityValue[parameter['key']]
+          //         }\n`
+          //       );
+          //       // console.error(
+          //       //   `[Error/selectRelevanceEntity] There doesn't handle type(${typeof item}).\n${entityValue[parameter['key']]
+          //       //   }\n`
+          //       // );
+          //       break;
+          //   }
+          // });
         } else {
-          if (entityValue[parameter['key']].id == parameter['value']) {
-            payload.push(entityValue);
-          }
+          // if (entityValue[parameter['key']].id == parameter['value']) {
+          //   payload.push(entityValue);
+          // }
         }
         break;
-      // case "undefined":
-      //   break
+      case "undefined":
+        break
       default:
-        Logger.error(
+        let _logger = Logger.error(
           'selectRelevanceEntity',
-          `There doesn't handle type(${typeofParameterKey}).\n${entityValue[parameter['key']]
-          }\n`
+          `There doesn't handle type(${typeofParameterKey}).\n${inspect(entity[parameter['key']], { "depth": 1 })}\n`,
+          { isPrint: Main.printMode !== "none" }
         );
-        // console.error(
-        //   `[Error/selectRelevanceEntity] There doesn't handle type(${typeofParameterKey}).\n${entityValue[parameter['key']]
-        //   }\n`
-        // );
+        if (envType == 'browser' && _logger['options']['isPrint'])
+          console.error(_logger['_str']);
         break;
     }
   });
-  // );
   return payload;
 };
 
